00cf3fdf3c48c88f1332aff505bd5652f72a8756
wll_OS <T2026105749910208@eduxiji.net>
2026-06-21 02:48:18 +0800

    fix: restore riscv sbi shutdown path

--- patch/polyhal/src/components/instruction/riscv64/shutdown.rs
+++ patch/polyhal/src/components/instruction/riscv64/shutdown.rs
@@ -1,32 +1,8 @@
 /// Call SBI_SHUTDOWN to close the machine. Exit qemu if you are using qemu.
 #[inline]
 pub fn shutdown() -> ! {
-    qemu_virt_finisher_pass();
-
-    let _ = sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
-
-    unsafe {
-        core::arch::asm!(
-            "ecall",
-            in("a7") sbi_rt::legacy::LEGACY_SHUTDOWN,
-            lateout("a0") _,
-        );
-    }
-
-    loop {
-        unsafe {
-            core::arch::asm!("wfi");
-        }
-    }
-}
-
-#[inline]
-fn qemu_virt_finisher_pass() {
-    const QEMU_VIRT_TEST_FINISHER: *mut u32 = 0x100000 as *mut u32;
-    const FINISHER_PASS: u32 = 0x5555;
-
-    unsafe {
-        QEMU_VIRT_TEST_FINISHER.write_volatile(FINISHER_PASS);
-    }
+    // sbi_rt::legacy::shutdown();
+    sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
+    unreachable!()
 }
  
