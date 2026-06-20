a6ca5a3b7ece6ae866ff109b06cef332bbfbec2c
wll_OS <T2026105749910208@eduxiji.net>
2026-06-21 00:53:13 +0800

    fix: use qemu finisher for riscv shutdown

--- patch/polyhal/src/components/instruction/riscv64/shutdown.rs
+++ patch/polyhal/src/components/instruction/riscv64/shutdown.rs
@@ -1,6 +1,8 @@
 /// Call SBI_SHUTDOWN to close the machine. Exit qemu if you are using qemu.
 #[inline]
 pub fn shutdown() -> ! {
+    qemu_virt_finisher_pass();
+
     let _ = sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
 
     unsafe {
@@ -15,6 +17,16 @@ pub fn shutdown() -> ! {
         unsafe {
             core::arch::asm!("wfi");
         }
+    }
+}
+
+#[inline]
+fn qemu_virt_finisher_pass() {
+    const QEMU_VIRT_TEST_FINISHER: *mut u32 = 0x100000 as *mut u32;
+    const FINISHER_PASS: u32 = 0x5555;
+
+    unsafe {
+        QEMU_VIRT_TEST_FINISHER.write_volatile(FINISHER_PASS);
     }
 }
  
