3c145bcb1d84ba5ad6ba203f1a0bcafde64cb0d8
wll_OS <T2026105749910208@eduxiji.net>
2026-06-21 16:43:52 +0800

    Restore legacy SBI shutdown fallback

--- patch/polyhal/src/components/instruction/riscv64/shutdown.rs
+++ patch/polyhal/src/components/instruction/riscv64/shutdown.rs
@@ -1,8 +1,19 @@
 /// Call SBI_SHUTDOWN to close the machine. Exit qemu if you are using qemu.
 #[inline]
 pub fn shutdown() -> ! {
-    // sbi_rt::legacy::shutdown();
-    sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
-    unreachable!()
+    let _ = sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
+
+    unsafe {
+        core::arch::asm!(
+            "ecall",
+            in("a7") sbi_rt::legacy::LEGACY_SHUTDOWN,
+            lateout("a0") _,
+        );
+    }
+
+    loop {
+        unsafe {
+            core::arch::asm!("wfi");
+        }
+    }
 }
- 
